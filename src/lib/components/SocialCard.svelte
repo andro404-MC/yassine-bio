7a92d2dc1795b48bcd8b6f10deed93471f991ffe
Untemi <94703538+andro404-MC@users.noreply.github.com>
2023-12-01 21:29:39 +0000

    Update SocialCard.svelte

--- src/lib/components/SocialCard.svelte
+++ src/lib/components/SocialCard.svelte
@@ -1,29 +1,31 @@
 <script>
-  import { onMount } from "svelte";
+  import { onMount } from 'svelte';
+  import { fade } from 'svelte/transition'
+
   export let title;
   export let paragraph;
   export let bg;
   export let link;
   export let imgLink;
-  import { fade } from "svelte/transition";
-  let loaded = false
+  let added = false;
+
   onMount(() => {
     setTimeout(() => {
-      loaded = true;
+      added = true;
     }, 0.3);
   });
 </script>
-{#if loaded}
-<a target="_blank" in:fade={{duration:600}} href="{link}">
-  <div class="card bg-base-300 shadow-xl">
-    <div class="card-body p-5">
-      <h2 class="card-title">{title}</h2>
-      <p>{paragraph}</p>
-    </div>
-    <figure style="background:{bg};" class="p-2 sm:p-10">
-      <img height="48" width="144" src="{imgLink}" alt="logo" />
-    </figure>
-  </div>
-</a>
 
+{#if added}
+  <a transition:fade={{ duration: 300 }} href="{link}">
+    <div class="card bg-base-300 shadow-xl">
+      <div class="card-body p-5">
+        <h2 class="card-title">{title}</h2>
+        <p>{paragraph}</p>
+      </div>
+      <figure style="background:{bg};" class="p-2 sm:p-10">
+        <img height="48" width="144" src="{imgLink}" alt="logo" />
+      </figure>
+  </div>
+  </a>
 {/if}
