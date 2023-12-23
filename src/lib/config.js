092c1dce6f64cb5320971a31fba5dd7abac704f6
YassineBidamou <152397149+YassineBidamou@users.noreply.github.com>
2023-12-23 03:18:30 +0100

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,4 @@
-export let profileLink = "https://yassine.sirv.com/IMG_1233.JPG"
+export let profileLink = "https://yassine.sirv.com/ana.png"
 export let favicon = "https://yassine.sirv.com/gyftg.jpg.png"
 export let name = "Yassine Bidamou"
 export let kofiLink = "https://paypal.me/YassineBidamou"
